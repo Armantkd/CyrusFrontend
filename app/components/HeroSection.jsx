65203a78e9ed7191fdf48e094f5c5c37ae7e93c2
Arman <armantkidd@gmail.com>
2024-04-19 02:50:29 -0400

    First commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-12 place-self-center text-center sm:text-left justify-self-start"
+        > 
+          <h1 className="text-green-700 mb-4 text-4xl sm:text-5xl md:text-6xl lg:text-8xl lg:leading-normal font-bold">
+             
+            <TypeAnimation
+              sequence={[
+                "Trade Wisely",
+                1000,
+                "Trade Efficiently",
+                1000,
+                "Trade Profitably",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
+          </p>
+          <div>
+            <Link
+              href="/contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-600 to-secondary-800 hover:bg-slate-200 text-white"
+            >
+                Contact
+        
+            </Link>
+          
+          </div>
+        </motion.div>
+        {/*
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+        {/ <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero-image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+            </div> </section> 
+        </motion.div> */}
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
